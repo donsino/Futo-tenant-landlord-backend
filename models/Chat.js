9ed3b41ac88563cc1294dd1dd7dee281f3dbb7e7
uabochi <uabochi@gmail.com>
2024-12-01 20:51:33 +0100

    Model: Modified the chat model

--- models/Chat.js
+++ models/Chat.js
@@ -1,20 +1,5 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-const ChatSchema = new mongoose.Schema({
-    chatId: { type: String, required: true }, // Unique ID for the chat room
-    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Landlord and Student
-    messages: [
-        {
-            sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-            message: { type: String, required: true },
-            timestamp: { type: Date, default: Date.now },
-        },
-    ],
-});
-
-module.exports = mongoose.model('Chat', ChatSchema);
-=======
 // Here is the mdels for the chatting between Landlord and Tenant
 
 const chatSchema = new mongoose.Schema({
@@ -25,4 +10,3 @@ const chatSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Chat', chatSchema);
->>>>>>> b6dc0a69cfbae14191e940afea3222497bf8270c
